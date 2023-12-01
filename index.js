e0194ca7a9cb169031b26b03ee861fc82312656d
anav5704 <anavkki@gmail.com>
2023-12-01 15:01:49 +1200

    feat: add express server and endpoint

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { DiscussServiceClient } = require("@google-ai/generativelanguage")
 const { GoogleAuth } = require("google-auth-library")
+const express = require("express")
+const { status } = require("express/lib/response")
 require("dotenv").config()
+
+const app = express()
+app.use(express.json())
 
 const MODEL_NAME = "models/chat-bison-001"
 const API_KEY = process.env.API_KEY
@@ -9,7 +14,7 @@ const client = new DiscussServiceClient({
     authClient: new GoogleAuth().fromAPIKey(API_KEY),
 })
 
-const AI = async () => {
+const BATMAN = async (prompt) => {
     const result = await client.generateMessage({
         model: MODEL_NAME,
         temperature: 0.5,
@@ -24,11 +29,19 @@ const AI = async () => {
                     },
                 },
             ],
-            messages: [{ content: "Why do criminals fear you?" }],
+            messages: [{ content: prompt }],
         },
-    });
+    })
 
-    console.log(result[0].candidates[0].content);
+    return result[0].candidates[0].content
 }
 
-AI()
+app.post("/BATMAN", async (req, res) => {
+    const { prompt } = req.body
+    const data = await BATMAN(prompt)
+    res.json({status: 200, response: data})
+})
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000")
+})
